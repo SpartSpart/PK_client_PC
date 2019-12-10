2c796e68e5ff442b53f1ce30d5247ac16344939d
SpartSpart <spart_85@inbox.ru>
2019-12-10 12:07:47 +0300

    Spring IO. Init. Just auto created the main structure.

---
+++ src/main/java/ru/spart/PK_client_PC/PkClientPcApplication.java
@@ -0,0 +1,13 @@
+package ru.spart.PK_client_PC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkClientPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkClientPcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spart/PK_client_PC/PkClientPcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spart.PK_client_PC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkClientPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
