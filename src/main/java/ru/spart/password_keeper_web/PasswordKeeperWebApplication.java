113cfa6eabb116ee3e4f7975a3a543fe2162ce9b
Pamela <Pamela@PAMELA->
2020-01-18 22:06:29 +0300

    Init

---
+++ src/main/java/ru/spart/password_keeper_web/PasswordKeeperWebApplication.java
@@ -0,0 +1,13 @@
+package ru.spart.password_keeper_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordKeeperWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordKeeperWebApplication.class, args);
+	}
+
+}
